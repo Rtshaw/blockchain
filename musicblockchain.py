38cbe7c2a745585b54e2fb8d96c78c0753dbcd07
rrshaw <haibara80143@gmail.com>
2018-02-11 07:39:07 +0800

    Instantiate two chain

--- musicblockchain.py
+++ musicblockchain.py
@@ -12,10 +12,10 @@ from flask import Flask, jsonify, request
 class Blockchain:
     def __init__(self):
         self.current_transactions = []
-        self.chain = []
+        self.main_chain = []
         self.nodes = set()
 
-        # 创建创世块
+        # 創建主鏈創世區塊
         self.new_block(previous_hash='1', proof=100)
 
     def register_node(self, address: str) -> None:
@@ -28,19 +28,19 @@ class Blockchain:
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
 
-    def valid_chain(self, chain: List[Dict[str, Any]]) -> bool:
+    def valid_chain(self, main_chain: List[Dict[str, Any]]) -> bool:
         """
         Determine if a given blockchain is valid
 
-        :param chain: A blockchain
+        :param main_chain: A blockchain
         :return: True if valid, False if not
         """
 
-        last_block = chain[0]
-        current_index = 1
+        last_block = main_chain[0]
+        current_main_index = 1
 
-        while current_index < len(chain):
-            block = chain[current_index]
+        while current_main_index < len(main_chain):
+            block = main_chain[current_main_index]
             print(f'{last_block}')
             print(f'{block}')
             print("\n-----------\n")
@@ -53,6 +53,35 @@ class Blockchain:
                 return False
 
             last_block = block
+            current_main_index += 1
+
+        return True
+    
+    def valid_music_chain(self, music_chain: List[Dict[str, Any]]) -> bool:
+        """
+        Determine if a given blockchain is valid
+
+        :param music_chain: A blockchain
+        :return: True if valid, False if not
+        """
+
+        last_music_block = music_chain[0]
+        current_index = 1
+
+        while current_index < len(music_chain):
+            music_block = music_chain[current_index]
+            print(f'{last_music_block}')
+            print(f'{music_block}')
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if music_block['previous_hash'] != self.hash(last_music_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_music_proof(last_music_block['music_proof'], music_block['music_proof']):
+                return False
+
+            last_music_block = music_block
             current_index += 1
 
         return True
@@ -69,31 +98,87 @@ class Blockchain:
         new_chain = None
 
         # We're only looking for chains longer than ours
-        max_length = len(self.chain)
+        max_length = len(self.main_chain)
 
         # Grab and verify the chains from all the nodes in our network
         for node in neighbours:
             response = requests.get(f'http://{node}/chain')
 
             if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
+                mainlength = response.json()['mainlength']
+                main_chain = response.json()['main_chain']
 
-                # Check if the length is longer and the chain is valid
-                if length > max_length and self.valid_chain(chain):
-                    max_length = length
-                    new_chain = chain
+                # Check if the length is longer and the main_chain is valid
+                if mainlength > max_length and self.valid_chain(main_chain):
+                    max_length = mainlength
+                    new_chain = main_chain
 
-        # Replace our chain if we discovered a new, valid chain longer than ours
+        # Replace our main_chain if we discovered a new, valid main_chain longer than ours
         if new_chain:
-            self.chain = new_chain
+            self.main_chain = new_chain
             return True
 
         return False
 
+    def resolve_music_conflicts(self) -> bool:
+        """
+        共识算法解决冲突
+        使用网络中最长的链.
+
+        :return:  如果链被取代返回 True, 否则为False
+        """
+
+        neighbours = self.nodes
+        new_music_chain = None
+
+        # We're only looking for chains longer than ours
+        max_length = len(self.music_chain)
+
+        # Grab and verify the chains from all the nodes in our network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                musiclength = response.json()['musiclength']
+                music_chain = response.json()['music_chain']
+
+                # Check if the length is longer and the main_chain is valid
+                if musiclength > max_length and self.valid_chain(music_chain):
+                    max_length = musiclength
+                    new_music_chain = music_chain
+
+        # Replace our main_chain if we discovered a new, valid main_chain longer than ours
+        if new_music_chain:
+            self.music_chain = new_music_chain
+            return True
+
+        return False
+    
+    # 交易區塊（有交易紀錄）
+    def new_music_block(self, main_index, music_proof: int, previous_hash: Optional[str]) -> Dict[str, Any]:
+        
+        music_block = {
+            'index':len(self.music_chain)+1,
+            'main_index':main_index,
+            'timestamp':time(),
+            'transactions': self.current_transactions,
+            'music_proof': music_proof,
+            'previous_hash': previous_hash or self.hash(self.music_chain[-1]),
+        }
+        
+        # Reset the current list of transactions and music chain
+        self.current_transactions = []
+
+        print(len(self.music_chain)+1)
+        
+        self.music_chain.append(music_block)
+        return music_block
+
+    
+    # 創建音樂區塊（放音檔hash）（尚未放音檔hash）
     def new_block(self, proof: int, previous_hash: Optional[str]) -> Dict[str, Any]:
         """
-        生成新块
+        生成新區塊
 
         :param proof: The proof given by the Proof of Work algorithm
         :param previous_hash: Hash of previous Block
@@ -101,22 +186,24 @@ class Blockchain:
         """
 
         block = {
-            'index': len(self.chain) + 1,
+            'main_index': len(self.main_chain) + 1,
             'timestamp': time(),
-            'transactions': self.current_transactions,
             'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            'previous_hash': previous_hash or self.hash(self.main_chain[-1]),
         }
-
-        # Reset the current list of transactions
-        self.current_transactions = []
-
-        self.chain.append(block)
+        
+        
+        # 創立music-chain（交易區塊）的創世區塊
+        self.music_chain = []
+        self.new_music_block(len(self.main_chain)+1, previous_hash='1', music_proof=100)
+        
+        self.main_chain.append(block)
         return block
-
+    
+    # 交易紀錄
     def new_transaction(self, sender: str, recipient: str, amount: int) -> int:
         """
-        生成新交易信息，信息将加入到下一个待挖的区块中
+        生成新交易信息，信息将加入到下一个待挖的區塊中
 
         :param sender: Address of the Sender
         :param recipient: Address of the Recipient
@@ -129,11 +216,15 @@ class Blockchain:
             'amount': amount,
         })
 
-        return self.last_block['index'] + 1
+        return self.last_music_block['index'] + 1
 
     @property
     def last_block(self) -> Dict[str, Any]:
-        return self.chain[-1]
+        return self.main_chain[-1]
+    
+    @property
+    def last_music_block(self) -> Dict[str, Any]:
+        return self.music_chain[-1]
 
     @staticmethod
     def hash(block: Dict[str, Any]) -> str:
@@ -146,6 +237,18 @@ class Blockchain:
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+    
+    @staticmethod
+    def hash(music_block: Dict[str, Any]) -> str:
+        """
+        生成块的 SHA-256 hash值
+
+        :param block: Block
+        """
+        
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        music_block_string = json.dumps(music_block, sort_keys=True).encode()
+        return hashlib.sha256(music_block_string).hexdigest()
 
     def proof_of_work(self, last_proof: int) -> int:
         """
@@ -159,6 +262,19 @@ class Blockchain:
             proof += 1
 
         return proof
+    
+    def proof_of_music_work(self, last_music_proof: int) -> int:
+        """
+        简单的工作量证明:
+         - 查找一个 p' 使得 hash(pp') 以4个0开头
+         - p 是上一个块的证明,  p' 是当前的证明
+        """
+
+        music_proof = 0
+        while self.valid_music_proof(last_music_proof, music_proof) is False:
+            music_proof += 1
+
+        return music_proof
 
     @staticmethod
     def valid_proof(last_proof: int, proof: int) -> bool:
@@ -173,7 +289,15 @@ class Blockchain:
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+    
+    @staticmethod
+    def valid_music_proof(last_music_proof: int, music_proof: int) -> bool:
 
+        guess = f'{last_music_proof}{music_proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+    
+    
 
 # Instantiate the Node
 app = Flask(__name__)
@@ -183,16 +307,17 @@ node_identifier = str(uuid4()).replace('-', '')
 
 # Instantiate the Blockchain
 blockchain = Blockchain()
+print(type(blockchain.music_chain))
 
-
-@app.route('/mine', methods=['GET'])
-def mine():
+# 一般礦的的挖礦（交易區塊）music chain 
+@app.route('/<main_index>/mine', methods=['GET'])
+def mine(main_index):    
     # We run the proof of work algorithm to get the next proof...
-    last_block = blockchain.last_block
-    last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
+    last_music_block = blockchain.last_music_block
+    last_music_proof = last_music_block['music_proof']
+    music_proof = blockchain.proof_of_music_work(last_music_proof)
 
-    # 给工作量证明的节点提供奖励.
+    # 给工作量证明的節點提供獎勵.
     # 发送者为 "0" 表明是新挖出的币
     blockchain.new_transaction(
         sender="0",
@@ -200,24 +325,43 @@ def mine():
         amount=1,
     )
 
-    # Forge the new Block by adding it to the chain
-    block = blockchain.new_block(proof, None)
+    # Forge the new Block by adding it to the main_chain
+    music_block = blockchain.new_music_block(main_index, music_proof, None)
 
     response = {
         'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
+        'main_index':main_index,
+        'index': music_block['index'],
+        'transactions': music_block['transactions'],
+        'music_proof': music_block['music_proof'],
+        'previous_hash': music_block['previous_hash'],
     }
     return jsonify(response), 200
 
+# 音樂區塊（main chain）
+@app.route('/addmusic', methods=['GET'])
+def addmusic():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+        
+    block = blockchain.new_block(proof, None)
+    
+    response = {
+        'message' : "New Music Add",
+        'main_index' : block['main_index'],
+        'proof' : block['proof'],
+        'previous_hash' : block['previous_hash'],
+    }
+    
+    
+    return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
     values = request.get_json()
 
-    # 检查POST数据
+    # 檢查POST数据
     required = ['sender', 'recipient', 'amount']
     if not all(k in values for k in required):
         return 'Missing values', 400
@@ -232,10 +376,13 @@ def new_transaction():
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
+        'main_chain': blockchain.main_chain,
+        'music_chain':blockchain.music_chain,
+        'mainlength': len(blockchain.main_chain),
+        'musiclength':len(blockchain.music_chain),
     }
     return jsonify(response), 200
+    
 
 
 @app.route('/nodes/register', methods=['POST'])
@@ -262,13 +409,13 @@ def consensus():
 
     if replaced:
         response = {
-            'message': 'Our chain was replaced',
-            'new_chain': blockchain.chain
+            'message': 'Our main_chain was replaced',
+            'new_chain': blockchain.main_chain
         }
     else:
         response = {
-            'message': 'Our chain is authoritative',
-            'chain': blockchain.chain
+            'message': 'Our main_chain is authoritative',
+            'main_chain': blockchain.main_chain
         }
 
     return jsonify(response), 200
